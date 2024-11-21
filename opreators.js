5200590fe1ef0c8ead2ca45129b8bf8b35eb5a77
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-11-21 18:47:46 +0530

    Add files via upload

---
+++ opreators.js
@@ -0,0 +1,50 @@
+//opreators 
+//there are many type of opreators
+//1.arithmatic opreator
+//+,-,*,/,%,**
+//some examples:-
+// a=10,b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+//2.assignment opreators
+//+=,-=,*=,/=,**=
+//some examples:-
+// x=2,y=4;
+// console.log(x+=y);//x=x+y
+// console.log(x);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x**=y);
+
+//3.comparsion or conditional opreators
+//<,<=,>,>=,==,===,!=,!==
+// == :- only check the value
+// === :- check value and datatype
+//some examples
+// m=2,n=9;
+// console.log(m<n);
+// console.log(m<=n);
+// console.log(m>n);
+// console.log(m>=n);
+// console.log(m==n);
+// console.log(m===n);
+// console.log(m!=n);
+// console.log(m!==n);
+
+//4.logical opreator
+// && , || , !
+// some examples
+// p=1,q=0
+// console.log(p&&q);
+// console.log(p||q);
+// console.log(!p);
+// console.log(!q);
+
+
+
